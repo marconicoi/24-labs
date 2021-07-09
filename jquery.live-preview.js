4fafbe2013c5a6755f4d405a61bdf65d8f7104ee
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-07-09 18:13:24 -0300

    Update jquery.live-preview.js

--- jquery.live-preview.js
+++ jquery.live-preview.js
@@ -1,21 +1,45 @@
 $(function(){
 	$(document).on('keyup',':input',function() {
-		var self = $(this);
-		$('[data-bind=' + $(this).attr('id') + ']').not('a').html($(this).val()).each(function() {
-			var html = '';
-			var sep = false;
-			var container = $(this).closest('[data-separator]');
-			container.find('[data-bind]').each(function() {
-				if ($(this).html() != '' && $(this).is(':visible')) {
-					if (sep) html += '<span>' + container.attr('data-separator') + '</span>';
-					sep = true;
-				}
-				html += $(this).get(0).outerHTML;
+		let input=$(this);
+		$('[data-24-bind*="#'+$(this).attr('id')+'"]').each(function(){
+			let self=$(this);
+			let formula=$(this).data('24-bind');
+			let terms=formula.split(/[,;:\.\s()]/);
+			terms.forEach(function(e){
+				let v=$(e).val();
+				if(v===undefined) v=e;
+				formula=formula.replace(e,v);
 			});
-			container.html(html);
+			formula=formula.replaceAll('()','').replace(/^[,;:\.\s]+/,'').replace(/[,;:\.\s]+$/,'');
+			if(self.is(':input')) self.val(formula);
+			else self.html(formula);
+			if(self.is('a')){
+				if(input.is('[type=tel]')) self.attr('href','tel://'+formula);
+				else if(input.is('[type=email]')) self.attr('href','mailto://'+formula);
+			}
 		});
-		$('[data-bind=' + $(this).attr('id') + ']').filter('a').each(function() {
-			$(this).attr('href', self.attr('type') == 'email' ? 'mailto:' + self.val() : self.attr('type') == 'tel' ? 'tel:' + self.val() : '');
+		$('[data-24-calc*="#'+$(this).attr('id')+'"]').each(function(){
+			let self=$(this);
+			let formula=$(this).data('24-calc');
+			let terms=formula.replaceAll(' ','').split(/[*+-/]/);
+			terms.forEach(function(e){
+				let v=$(e).val();
+				if(v===undefined) v=e;
+				formula=formula.replace(e,v);
+			});
+			formula=eval(formula).toLocaleString();
+			if(self.is(':input')) self.val(formula);
+			else self.html(formula);
 		});
 	}).filter('[value!=""]').keyup();
+	$(document).on('click',':checkbox',function(){
+		if($(this).is(':checked')){
+			$('[data-24-show="#'+$(this).attr('id')+'"]').show();
+			$('[data-24-hide="#'+$(this).attr('id')+'"]').hide();
+		}
+		else{
+			$('[data-24-show="#'+$(this).attr('id')+'"]').hide();
+			$('[data-24-hide="#'+$(this).attr('id')+'"]').show();
+		}
+	}).click();
 });

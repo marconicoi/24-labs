d81d5f982ccbefaac5f4d47df81d417e2c504784
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-08-09 14:07:59 -0300

    Update jquery.videos.js

--- jquery.videos.js
+++ jquery.videos.js
@@ -3,20 +3,28 @@ $(function(){
 		window._wq=window._wq||[];
 		const vid=$.grep(this.className.split(' '),(v,i)=>v.indexOf('wistia_async_')===0).join().replace('wistia_async_','');
 		let chapters=[];
-		$(this).find('chapter').each(function(){
-			chapters.push({
-				start: parseFloat($(this).attr('start')),
-				class: $(this).attr('class')
-			});
+		$('[data-dd-wistiaid="'+vid+'"]').filter('[data-dd-currentat]').each(function(){
+			const t=parseFloat($(this).data('dd-currentat'));
+			const i=chapters.findIndex((ch)=>ch.start==t);
+			if(i==-1) chapters.push({
+					start: t,
+					elements: $(this)
+				});
+			else chapters[i].elements=chapters[i].elements.add($(this));
 		});
-		chapters.reverse();
+		chapters.sort((a,b)=>b.start-a.start);
 		_wq.push({id:vid,onReady:function(video){
 			video.bind('timechange',function(t){
+				let se=true;
 				chapters.forEach(function(ch){
-					$('.'+ch.class+'[data-dd-chapterclass='+vid+']').removeClass(ch.class);
+					if(se&&ch.start<=t){
+						ch.elements.addClass('w--current');
+						se=false;
+					}
+					else{
+						ch.elements.removeClass('w--current');
+					}
 				});
-				const cls=chapters.find((ch)=>ch.start<=t).class;
-				$('[data-dd-chapterclass='+vid+']').addClass(cls);
 			});
 		}});
 	});

894b3f856c5ae6e4974217be15b714ba88831ba9
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-08-13 09:42:10 -0300

    Update jquery.live-preview.js

--- jquery.live-preview.js
+++ jquery.live-preview.js
@@ -67,7 +67,8 @@ $(function(){
 	function __dt__update(el){
 		const tm=el.data('dd-timestamp');
 		const ct=new Date();
-		const dt=new Date(isNaN(tm)?tm:tm*1000);
+		const dt=new Date(isNaN(tm)?tm.replace('T',' ').replace('Z',' GMT'):tm*1000);
+		if(isNaN(dt.getTime())) return false;
 		const cy=ct.getFullYear();
 		const dy=dt.getFullYear();
 		const cd=cy*10000+ct.getMonth()*100+ct.getDate();
@@ -100,5 +101,76 @@ $(function(){
 		if(ti>0) setTimeout(function(){
 			__dt__update(el);
 		},ti);
+	}
+	$('[data-dd-autoload]:not(:input,form)').each(function(){
+		const self=$(this);
+		const template=self.html();
+		const loop=self.is('[data-dd-autoloop]')?self.data('dd-autoloop'):false;
+		$.getJSON(self.data('dd-autoload'),function(json){
+			__auto__target__populate(json,self,template,loop);
+		});
+		self.removeAttr('data-dd-autoload').removeAttr('data-dd-autoloop').html('');
+	});
+	$(':input[data-dd-autoload]').each(function(){
+		const self=$(this);
+		const url_base=self.data('dd-autoload');
+		const target=$(self.attr('target'));
+		const template=target.html();
+		const loop=target.is('[data-dd-autoloop]')?target.data('dd-autoloop'):false;
+		$(this).change(function(){
+			$.getJSON(url_base.replaceAll(/\{\{[^\}]+\}\}/g,(s)=>eval(s.replaceAll(/[\{\}\s]/g,''))),function(json){
+				__auto__target__populate(json,target,template,loop);
+			});
+		});
+		target.removeAttr('data-dd-autoloop').html('');
+		self.removeAttr('data-dd-autoload').change();
+	});
+	$('form[data-dd-autoload]').each(function(){
+		const self=$(this);
+		const url_base=self.data('dd-autoload');
+		const template=self.html();
+		const loop=false;
+		$(this).submit(function(){
+			$.getJSON(url_base.replaceAll(/\{\{[^\}]+\}\}/g,(s)=>eval(s.replaceAll(/[\{\}\s]/g,''))),self.serialize(),function(json){
+				__auto__target__populate(json,self,template,loop);
+			});
+			return false;
+		});
+		target.removeAttr('data-dd-autoloop').html('');
+		self.removeAttr('data-dd-autoload').change();
+	});
+	function __auto__target__populate(json,target,template,loop){
+		let html='';
+		let stm=loop?'json.'+loop:false;
+		let offset=0;
+		let limit=1000;
+		if(loop&&loop.includes(':')){
+			let prts=loop.split(':');
+			stm='json.'+prts[0];
+			if(prts.length==2) limit=prts[1];
+			else{
+				offset=prts[1];
+				limit=prts[2];
+			}
+		}
+		if(stm){
+			eval(stm).forEach(function(obj){
+				if(offset>0) offset--;
+				else if(limit-->0){
+					html+=template.replaceAll(/\{\{\s*[\w\.]+\s*\}\}/g,(s)=>eval('obj.'+s.replaceAll(/[\{\}\s]/g,'')));
+				}
+			});
+		}
+		else{
+			html=template.replaceAll(/\{\{\s*[\w\.]+\s*\}\}/g,(s)=>eval('json.'+s.replaceAll(/[\{\}\s]/g,'')));
+		}
+		target.html(html);
+		target.find('[data-dd-autocontent]').each(function(){
+			$(this).html($(this).data('dd-autocontent')).removeAttr('data-dd-autocontent');
+		});
+		target.find(':input').change();
+		target.find('[data-dd-timestamp]').change(function(){
+			__dt__update($(this));
+		}).change();
 	}
 });

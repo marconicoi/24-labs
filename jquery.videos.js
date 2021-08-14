404dbd3b3b8152d9716060d9b684b3509aba4ae4
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-08-14 03:07:41 -0300

    Update jquery.videos.js

--- jquery.videos.js
+++ jquery.videos.js
@@ -6,9 +6,17 @@ $(function(){
 			video.bind('timechange',function(t){
 				$('[data-dd-wistiaid="'+vid+'"]').filter('[data-dd-activate]').each(function(){
 					const cls=$(this).is('[data-dd-activeclass]')?$(this).data('dd-activeclass'):'w--active';
-					const ta=parseFloat($(this).data('dd-activate'));
-					const td=$(this).is('[data-dd-deactivate]')?parseFloat($(this).data('dd-deactivate')):null;
-					if(ta<=t&&(td===null||td>t)) $(this).addClass(cls);
+					let ta=$(this).data('dd-activate');
+					if(typeof ta==='string'&&ta.includes(':')){
+						let k=1/60;
+						ta=ta.split(':').reverse().reduce((a,c)=>a*1+c*(k*=60),0);
+					}
+					let td=$(this).is('[data-dd-deactivate]')?$(this).data('dd-deactivate'):null;
+					if(typeof td==='string'&&td.includes(':')){
+						let k=1/60;
+						td=td.split(':').reverse().reduce((a,c)=>a*1+c*(k*=60),0);
+					}
+					if(parseFloat(ta)<=t&&(td===null||parseFloat(td)>t)) $(this).addClass(cls);
 					else $(this).removeClass(cls);
 				});
 			});

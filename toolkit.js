05fab2ee2b04d984ffd0db349dc631b259f5ab7b
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-08-21 21:30:23 -0300

    Update toolkit.js

--- toolkit.js
+++ toolkit.js
@@ -11,12 +11,19 @@ document.addEventListener('DOMContentLoaded',function(){
 	document.querySelectorAll('[data-dd-split]').forEach(function(el){
 		const s=this.getAttribute('data-dd-split');
 		if(s.match(/\.\[\d+\]/)){
-			this.innerHTML=this.innerHTML.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
+			if(this instanceof HTMLInputElement) this.value=this.value.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
+			else this.innerHTML=this.innerHTML.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
 		}
 	});
 	document.querySelectorAll('[data-dd-crop]').forEach(function(el){
 		let o=parseInt(this.getAttribute('data-dd-crop'));
-		if(o>0) this.innerHTML=this.innerHTML.substr(0,o);
-		else this.innerHTML=this.innerHTML.substr(o);
+		if(this instanceof HTMLInputElement){
+			if(o>0) this.value=this.value.substr(0,o);
+			else this.value=this.value.substr(o);
+		}
+		else{
+			if(o>0) this.innerHTML=this.innerHTML.substr(0,o);
+			else this.innerHTML=this.innerHTML.substr(o);
+		}
 	});
 });

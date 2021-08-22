2a386543866299f350647a9e199bb2c1bb5ac10a
Marconi Coimbra <marconi.coimbra@gmail.com>
2021-08-21 21:56:14 -0300

    Update toolkit.js

--- toolkit.js
+++ toolkit.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded',function(){
-	document.querySelectorAll('body *[lang=no]:not(script):not(noscript):not(style)').forEach(function(el){
+	document.querySelectorAll('[data-dd-split]').forEach(function(el){
+		const s=el.getAttribute('data-dd-split');
+		if(s.match(/.\[\d+\]/)){
+			if(el instanceof HTMLInputElement) el.value=el.value.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
+			else el.innerHTML=el.innerHTML.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
+		}
+	});
+	document.querySelectorAll('[data-dd-crop]').forEach(function(el){
+		let o=parseInt(el.getAttribute('data-dd-crop'));
+		if(el instanceof HTMLInputElement){
+			if(o>0) el.value=el.value.substr(0,o);
+			else el.value=el.value.substr(o);
+		}
+		else{
+			if(o>0) el.innerHTML=el.innerHTML.substr(0,o);
+			else el.innerHTML=el.innerHTML.substr(o);
+		}
+	});
+	document.querySelectorAll('[data-dd-regex]').forEach(function(el){
+		const r=new RegExp(el.getAttribute('data-dd-regex'));
+		if(el instanceof HTMLInputElement) el.value=el.value.replace(r,'$1');
+		else el.innerHTML=el.innerHTML.replace(r,'$1');
+	});
+	document.querySelectorAll('body *:not(script):not(noscript):not(style)').forEach(function(el){
 		if(el.hasChildNodes()){
 			el.childNodes.forEach(function(ch){
 				if(ch.nodeType==document.TEXT_NODE){
@@ -7,28 +30,5 @@ document.addEventListener('DOMContentLoaded',function(){
 				}
 			});
 		}
-	});
-	document.querySelectorAll('[data-dd-split]').forEach(function(el){
-		const s=this.getAttribute('data-dd-split');
-		if(s.match(/\.\[\d+\]/)){
-			if(this instanceof HTMLInputElement) this.value=this.value.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
-			else this.innerHTML=this.innerHTML.split(s.substr(0,1))[parseInt(s.substr(2).replace(']',''))];
-		}
-	});
-	document.querySelectorAll('[data-dd-crop]').forEach(function(el){
-		let o=parseInt(this.getAttribute('data-dd-crop'));
-		if(this instanceof HTMLInputElement){
-			if(o>0) this.value=this.value.substr(0,o);
-			else this.value=this.value.substr(o);
-		}
-		else{
-			if(o>0) this.innerHTML=this.innerHTML.substr(0,o);
-			else this.innerHTML=this.innerHTML.substr(o);
-		}
-	});
-	document.querySelectorAll('[data-dd-regex]').forEach(function(el){
-		const r=new RegExp(this.getAttribute('data-dd-regex'));
-		if(this instanceof HTMLInputElement) this.value=this.value.replace(r,'$1');
-		else this.innerHTML=this.innerHTML.replace(r,'$1');
 	});
 });
